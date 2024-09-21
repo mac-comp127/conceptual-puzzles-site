5b0a2bb3d159b3d5f7c44f00985015c6cc43892e
Paul Cantrell <cantrell@pobox.com>
2024-09-20 21:15:20 -0500

    Added autoreload

---
+++ app/javascript/autoreload.js
@@ -0,0 +1,27 @@
+// Presence of class="autoreload" anywhere on the page triggers autoreload behavior
+function findAutoreload() {
+    return document.querySelector(".autoreload")
+}
+
+var refreshTimer;
+
+// Clear any existing timer before visiting a new page
+document.addEventListener("turbo:visit", (event) => {
+    if (refreshTimer) {
+        clearTimeout(refreshTimer);
+        refreshTimer = null;
+    }
+});
+
+// Check page for autoreload whenever content changes
+document.addEventListener("turbo:load", (event) => {
+    if (findAutoreload()) {
+        refreshTimer = setTimeout(
+            () => {
+                refreshTimer = null;
+                Turbo.visit(event.detail.url);
+            },
+            1000
+        );
+    }
+});
