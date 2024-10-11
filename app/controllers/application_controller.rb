ba77f388cad6ec76f924c9a8cda94ceb94f74260
Paul Cantrell <cantrell@pobox.com>
2024-10-11 00:36:45 -0500

    Allowed slightly older browsers
    For Ruta!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  allow_browser versions: { safari: 15.4, chrome: 105, firefox: 121, opera: 91, ie: false }
 
   def require_login!(user_type)
     if current_user&.is_a?(user_type)
