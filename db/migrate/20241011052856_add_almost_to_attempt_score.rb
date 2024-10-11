cf8c52af05168d4fea8614a77afab444419870b2
Paul Cantrell <cantrell@pobox.com>
2024-10-11 00:38:06 -0500

    Older PostgreSQL doesn't allow alter enum in transaction

--- db/migrate/20241011052856_add_almost_to_attempt_score.rb
+++ db/migrate/20241011052856_add_almost_to_attempt_score.rb
@@ -1,4 +1,6 @@
 class AddAlmostToAttemptScore < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+
   def up
     execute "ALTER TYPE attempt_score ADD VALUE IF NOT EXISTS 'almost' BEFORE 'full_credit'"
   end
